30f45f2e06f451b832f2932ca4493ce7ec9fed10
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-01-09 08:23:56 -0600

    perf: tailwind LSP results are cached to be returned from WinScrolled event

--- lua/colorizer/tailwind.lua
+++ lua/colorizer/tailwind.lua
@@ -7,39 +7,37 @@
 local M = {}
 
 local utils = require("colorizer.utils")
-
 local tw_ns_id = require("colorizer.constants").namespace.tailwind_lsp
-local state = {}
 
-local function clear_ns(bufnr)
-  vim.api.nvim_buf_clear_namespace(bufnr, tw_ns_id, 0, -1)
-  state[bufnr].client = nil
-end
+local buf_state = {}
 
 --- Cleanup tailwind variables and autocmd
 ---@param bufnr number|nil: buffer number (0 for current)
 function M.cleanup(bufnr)
   bufnr = utils.bufme(bufnr)
-  if state[bufnr] and state[bufnr].au_id and state[bufnr].au_id[1] then
-    for _, au_id in ipairs(state[bufnr].au_id) do
+  if buf_state[bufnr] and buf_state[bufnr].au_id and buf_state[bufnr].au_id[1] then
+    for _, au_id in ipairs(buf_state[bufnr].au_id) do
       pcall(vim.api.nvim_del_autocmd, au_id)
     end
   end
-  clear_ns(bufnr)
+  vim.api.nvim_buf_clear_namespace(bufnr, tw_ns_id, 0, -1)
+  for k, _ in pairs(buf_state[bufnr]) do
+    buf_state[bufnr][k] = nil
+  end
 end
 
-local function highlight_tailwind(bufnr, ud_opts, add_highlight)
-  if not state[bufnr] or not state[bufnr].client or not state[bufnr].client.request then
+local function highlight(bufnr, ud_opts, add_highlight)
+  if not buf_state[bufnr] or not buf_state[bufnr].client or not buf_state[bufnr].client.request then
     return
   end
-  state[bufnr].document_params = state[bufnr].document_params
+  buf_state[bufnr].document_params = buf_state[bufnr].document_params
     or { textDocument = vim.lsp.util.make_text_document_params(bufnr) }
-  state[bufnr].client.request(
+  buf_state[bufnr].client.request(
     "textDocument/documentColor",
-    state[bufnr].document_params,
+    buf_state[bufnr].document_params,
     function(err, results, _, _)
       if err ~= nil then
-        vim.api.nvim_err_writeln("tailwind.highlight_tailwind: Error: " .. err)
+        vim.api.nvim_err_writeln("tailwind.highlight: Error: " .. err)
       end
       if err == nil and results ~= nil then
         local data, line_start, line_end = {}, nil, nil
@@ -73,6 +71,7 @@ local function highlight_tailwind(bufnr, ud_opts, add_highlight)
         end
         line_start = line_start or 0
         line_end = line_end and (line_end + 2) or -1
+        buf_state[bufnr].data = data
         add_highlight(bufnr, tw_ns_id, line_start, line_end, data, ud_opts, { tailwind_lsp = true })
       end
     end
@@ -86,18 +85,28 @@ end
 ---@param add_highlight function
 ---@param on_detach function
 ---@return boolean|nil
-function M.setup_lsp_colors(bufnr, ud_opts, buf_local_opts, add_highlight, on_detach)
+function M.lsp_highlight(
+  bufnr,
+  ud_opts,
+  buf_local_opts,
+  add_highlight,
+  on_detach,
+  line_start,
+  line_end
+)
   if not vim.api.nvim_buf_is_valid(bufnr) then
     return
   end
-  state[bufnr] = state[bufnr] or {}
-  state[bufnr].au_id = state[bufnr].au_id or {}
+  buf_state[bufnr] = buf_state[bufnr] or {}
+  buf_state[bufnr].au_id = buf_state[bufnr].au_id or {}
 
-  if vim.version().minor >= 8 and not state[bufnr].client or state[bufnr].client.is_stopped() then
+  if
+    vim.version().minor >= 8 and not buf_state[bufnr].client or buf_state[bufnr].client.is_stopped()
+  then
     -- create the autocmds so tailwind colors only activate when tailwindcss lsp is active
-    if not state[bufnr].au_created then
+    if not buf_state[bufnr].au_created then
       vim.api.nvim_buf_clear_namespace(bufnr, tw_ns_id, 0, -1)
-      state[bufnr].au_id[1] = vim.api.nvim_create_autocmd("LspAttach", {
+      buf_state[bufnr].au_id[1] = vim.api.nvim_create_autocmd("LspAttach", {
         group = buf_local_opts.__augroup_id,
         buffer = bufnr,
         callback = function(args)
@@ -105,26 +114,26 @@ function M.setup_lsp_colors(bufnr, ud_opts, buf_local_opts, add_highlight, on_de
           if ok and client then
             if
               client.name == "tailwindcss"
-              and client:supports_method("textDocument/documentColor", bufnr)
+              and client.supports_method("textDocument/documentColor", bufnr)
             then
-              state[bufnr].client = client
-              highlight_tailwind(bufnr, ud_opts, add_highlight)
+              buf_state[bufnr].client = client
+              highlight(bufnr, ud_opts, add_highlight)
             end
           end
         end,
       })
       -- make sure the autocmds are deleted after lsp server is closed
-      state[bufnr].au_id[2] = vim.api.nvim_create_autocmd("LspDetach", {
+      buf_state[bufnr].au_id[2] = vim.api.nvim_create_autocmd("LspDetach", {
         group = buf_local_opts.__augroup_id,
         buffer = bufnr,
         callback = function()
           on_detach(bufnr)
         end,
       })
-      state[bufnr].au_created = true
+      buf_state[bufnr].au_created = true
     end
 
-    clear_ns(bufnr)
+    vim.api.nvim_buf_clear_namespace(bufnr, tw_ns_id, 0, -1)
 
     local ok, client = pcall(function()
       local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "tailwindcss" })
@@ -137,14 +146,31 @@ function M.setup_lsp_colors(bufnr, ud_opts, buf_local_opts, add_highlight, on_de
       return
     end
 
-    state[bufnr].client = client
-    highlight_tailwind(bufnr, ud_opts, add_highlight)
+    buf_state[bufnr].client = client
+    highlight(bufnr, ud_opts, add_highlight)
 
     return true
   end
 
-  if state[bufnr].client then
-    highlight_tailwind(bufnr, ud_opts, add_highlight)
+  if buf_state[bufnr].client then
+    if
+      buf_state[bufnr].data
+      and not buf_state[bufnr].data_highlighted
+      and buf_local_opts.__event == "WinScrolled"
+    then
+      add_highlight(
+        bufnr,
+        tw_ns_id,
+        line_start,
+        line_end,
+        buf_state[bufnr].data,
+        ud_opts,
+        { tailwind_lsp = true }
+      )
+      buf_state[bufnr].data_highlighted = true
+    else
+      highlight(bufnr, ud_opts, add_highlight)
+    end
   end
 end
 
